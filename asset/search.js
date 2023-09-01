cac31f43c18576cfefa778da535e1af1da5eded6
Amimul Ihsan Zubair <86657812+aizubair21@users.noreply.github.com>
2023-09-01 16:23:17 +0600

    Update Filter
    > Search by conditional
    
    > if you type on search input bu not specify any filter condition. it's show you an alert to select condition.
    
    > as soon as you select a condition it run the conditional function itself.
    
    > when press in filter button filter box appear near to button. if select any of conditional option filter box instantly hidden.
    
    > if you forgate to hide the filter box, when you type on search bar. the filter box instantly hidden. and run the funtion.
    
    > when you specify a filter condition ti's show you an alert.
    
    > it have now thee kinds of filter option. but only database filter is now fixed.
    
    > search show an live search. for this you have to select Database filter from filter option.
    
    > Much more thing comes in future.

--- asset/search.js
+++ asset/search.js
@@ -4,56 +4,67 @@ var searchQuery = document.getElementsByClassName('query-count');
 let searchResult = document.getElementsByClassName('search-result');
 let queryCount = 0;
 let queryHtml = '';
+let testText = [
+    "Apples are delicious fruits",
+    "Birds chirp in the morning",
+    "Cats like to nap in the sun",
+    "Dogs are known for their loyalty",
+    "Elephants have large ears",
+    "Frogs croak near water bodies",
+    "Grapes can be red or green",
+    "Horses are often used for riding.",
+    "Ice cream is a popular dessert.",
+    "Jaguars are powerful predators.",
+    "Kangaroos have strong hind legs.",
+    "Lions are known as the  king of the jungle.",
+    "Monkeys swing from tree to tree.",
+    "Newts are small amphibians.",
+    "Owls are nocturnal birds.",
+    "Penguins live in cold climates.",
+    "Quokkas are cute and friendly animals.",
+    "Rabbits are known for their hopping.",
+    "Snakes can be both venomous and non - venomous.",
+    "Tigers are majestic big cats.",
+    "Umbrellabirds have unique crests.",
+    "Vultures are scavenger birds.",
+    "Whales are large marine mammals.",
+    "Xenophobes have an irrational fear of strangers.",
+    "Yellow can be a bright and cheerful color.",
+    "Zebras have distinctive black and white stripes",
+];
 
-searchInput.addEventListener("keyup", navSearch);
-searchInput.addEventListener('input', navSearch);
+// searchInput.addEventListener("keyup", navSearch);
+searchInput.addEventListener('input', initNav);
 
 window.addEventListener('keydown', event => {
     if (event.key == "/") {
         searchInput.focus();
     }
+
+    // console.log(event.key);
 });
 
-const testText = [
-    ["Apples are delicious fruits"],
-    ["Birds chirp in the morning"],
-    ["Cats like to nap in the sun"],
-    ["Dogs are known for their loyalty"],
-    ["Elephants have large ears"],
-    ["Frogs croak near water bodies"],
-    ["Grapes can be red or green"],
-    ["Horses are often used for riding."],
-    ["Ice cream is a popular dessert."],
-    ["Jaguars are powerful predators."],
-    ["Kangaroos have strong hind legs."],
-    ["Lions are known as the  king of the jungle."],
-    ["Monkeys swing from tree to tree."]
-    ["Newts are small amphibians."],
-    ["Owls are nocturnal birds."],
-    ["Penguins live in cold climates."],
-    ["Quokkas are cute and friendly animals."],
-    ["Rabbits are known for their hopping."],
-    ["Snakes can be both venomous and non - venomous."],
-    ["Tigers are majestic big cats."],
-    ["Umbrellabirds have unique crests."],
-    ["Vultures are scavenger birds."],
-    ["Whales are large marine mammals."],
-    ["Xenophobes have an irrational fear of strangers."],
-    ["Yellow can be a bright and cheerful color."],
-    ["Zebras have distinctive black and white stripes"]
-];
+// get data and  pass to nav search 
+function initNav(e) {
+    let val = e.target.value;
+    // openFilter();
+    searchByFilter();
+}
 
-function navSearch(e) {
-    if (e.target.value != "" && e.target.value != null) {
+//nav search
+function navSearch(val) {
+
+    if (val != "" && val != null) {
         searchResult[0].style.display = 'block';
         queryHtml = "";
 
-        console.log(e.target.value);
+        // console.log(e.target.value);
         testText.forEach((elm, index) => {
             // lowercase the vale 
-            let lowarCaseValue = elm[0].toLowerCase();
-            if (lowarCaseValue.startsWith(e.target.value)) {
-                console.log(elm);
+            let lowarCaseValue = elm.toLowerCase();
+            // console.log(lowarCaseValue);
+            if (lowarCaseValue.search(val) != -1) {
+                // console.log(elm);
                 //make html for found query 
                 queryHtml +=
                     `
@@ -63,18 +74,21 @@ function navSearch(e) {
                         </div>
                         
                         <div class="desc">
-                            <h5>${elm[0]}</h5>
+                            <h5>${elm}</h5>
                         </div>
                     </div>
                 `;
-                searchResult[0].innerHTML = queryHtml;
 
                 //show query count on search input right side
                 searchQuery[0].innerHTML = '';
                 // valu found 
+            } else {
+                //                 hide result div when nothing is found
+
             }
+            searchResult[0].innerHTML = queryHtml;
         })
-        console.log(queryCount);
+        // console.log(queryCount);
         // console.log(testText);
 
     } else {
@@ -85,9 +99,8 @@ function navSearch(e) {
     }
 }
 
-
 // filter modal opena and close 
-function openFilter() {
+function openFilter(condition) {
     let filterBtn = document.getElementsByClassName('filter-btn');
     let filterDiv = document.getElementsByClassName("filter-div");
 
@@ -97,34 +110,127 @@ function openFilter() {
         filterDiv[0].classList.add("filter-show");
     }
 
-}
+    if (condition == "hide") {
+        filterDiv[0].classList.remove("filter-show");
+    }
 
+}
 
 //fiter
 let filterItem = [];
 
+//add flter item functionality
 function filter(val) {
     // console.log(val);
     if (filterItem.length > 0) {
 
-        filterItem.map((itm, ind) => {
+        if (filterItem.indexOf(val) != -1) {
+            // console.log("matched");
+            filterItem.splice(filterItem.indexOf(val), 1); //splice method change the main array
+            filterAlert("You uncheck an filter item to filter !");
+        } else {
+            // console.log("this from else statement itm:" + itm + "  and val : " + val);
+            filterItem.push(val);
+            filterAlert("You check " + val + " to filter !");
 
-            if (itm == val) {
-                // console.log("matched");
-                filterItem.splice(ind, 1); //splice method change the main array
-            } else {
-                console.log("this from else statement :" + itm);
-                filterItem.push(val);
-            }
-
-            val = "";
-        })
+        }
 
     } else {
-        filterItem.push(val)
+        filterItem.push(val);
+        filterAlert("You check " + val + " to filter !");
 
     }
 
-    console.log("total item " + filterItem);
+    switch (val) {
+        case "db":
+            filterAlert("Go on Database Filtering !");
+            break;
+        case "cf":
+            filterAlert("Go on Content Filterint");
+            break;
 
+        default:
+            filterAlert("Default Filtering !");
+            break;
+    }
+    searchByFilter();
+    openFilter('hide');
+    // console.log("total item " + filterItem);
+
+}
+
+//filter alert 
+function filterAlert(alert_name) {
+
+    let p = document.createElement("p");
+    p.innerHTML = alert_name;
+    p.style.padding = 10 + "px";
+    p.classList.add('filter_alert');
+    p.style.backgroundColor = "rgb(211,211,211)";
+    p.style.position = "fixed";
+    p.style.top = 10 + "px";
+    p.style.right = 0 + "px";
+    p.style.width = 220 + "px";
+    p.style.height = "auto";
+    p.style.transition = "all linear .3s";
+
+    document.body.appendChild(p); //appending element in body tag
+    alert_name = "";
+
+    //set time out for 1 second to display none
+    setTimeout(() => {
+        if (document.getElementsByClassName('filter_alert')[0]) {
+            document.getElementsByClassName('filter_alert')[0].remove(); //remove element after one sec
+        }
+    }, 2000) //end of timeout function
+}
+
+// search by filter 
+function searchByFilter() {
+    if (filterItem.length > 0) {
+        filterItem.forEach(fItem => {
+            if (fItem == "df") {}
+
+            if (fItem == "db") {
+                navSearch(searchInput.value);
+                // filterAlert("Go On Database filtering !");
+            }
+
+            if (fItem == "cf") {
+
+            }
+
+        })
+
+    } else {
+        // window.addEventListener('keydown', event => {
+        //     if (event.key = "Backspace") {
+        //     }else{
+
+        //     }
+        // });
+        filterAlert("Please specify any one of filter !");
+        navSearch();
+    }
+
+    openFilter("hide");
+
+};
+
+//undow search by filer 
+function undowSearchByFilter() {
+
+}
+
+//show search value to input field
+function showValueToInputField(search_value) {
+    if (search_value == "") {
+        searchInput.value = "";
+        searchInput.focus();
+
+    } else {
+        searchInput.value = search_value;
+        searchInput.focus();
+
+    }
 }
